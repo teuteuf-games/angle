fc1b45bf45330eb6c53cdda162f0ae0c82420baa
emmapejko <emma.pejko@gmail.com>
2025-07-01 15:46:14 -0700

    Consent (#3)
    * always show manage consent button
    
    * only show ads to non premium users
    
    * always load ad script, set units based on sub
    
    * initialize ga after checking consent

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import { LoginModal } from './components/LoginModal';
 import { SocialLinks } from './components/SocialLinks';
 import useUser from './hooks/useUser';
 import AccountsUpdateComponent from './components/AccountsUpdateComponent';
+import { checkForConsent } from './ads';
 
 const Main = styled.div`
   flex: 1 0 auto;
@@ -150,6 +151,10 @@ function App() {
   const [win, setWin] = useState(false);
   const { user, setData, logout } = useUser();
 
+  useEffect(() => {
+    checkForConsent();
+  }, []);
+
   const deltaAngle = useMemo(
     () =>
       angle1 >= angle2 ? angle1 - angle2 : 2 * Math.PI - (angle2 - angle1),
@@ -204,81 +209,92 @@ function App() {
   };
 
   return (
-    <Main>
-      <BigContainer>
-        <AccountsUpdateComponent />
-        <ToastContainer
-          hideProgressBar
-          position="top-center"
-          transition={Flip}
-          autoClose={false}
-        />
-        <LogoContainer href={'/'}>
-          <Logo src={'/images/angle.svg'} alt="logo" />
-          <LogoText>ANGLE</LogoText>
-        </LogoContainer>
-        <IconContainer>
-          <HowToModal />
-          <StatsModal
-            end={end}
-            win={win}
-            guesses={guesses}
-            maxAttempts={MAX_GUESSES}
-            dayString={dayString}
-          ></StatsModal>
-          <LoginModal user={user} logout={logout} />
-        </IconContainer>
-        <Angle
-          angle1={angle1}
-          angle2={angle2}
-          largeArc={deltaAngle > Math.PI}
-        ></Angle>
-        <InputArea>
-          <Input
-            autoFocus
-            type="number"
-            pattern="\d*"
-            onChange={handleInput}
-            onKeyDown={handleEnter}
-            disabled={end}
-            value={guess}
+    <>
+      {!user?.premiumGames?.includes('angle') && (
+        <>
+          <div id="adngin-top_banner-0"></div>
+          <div id="adngin-video-0"></div>
+          <div id="adngin-adhesion-0"></div>
+        </>
+      )}
+      <Main>
+        <BigContainer>
+          <AccountsUpdateComponent />
+          <ToastContainer
+            hideProgressBar
+            position="top-center"
+            transition={Flip}
+            autoClose={false}
           />
-          <Button onClick={handleGuess} disabled={end}>
-            Guess!
-          </Button>
-        </InputArea>
-        <Attempts>
-          Attempts:{' '}
-          <span>
-            {guesses.length}/{MAX_GUESSES}
-          </span>
-        </Attempts>
-        <Guesses guesses={guesses} answer={answer} />
-        <SidebarAd>
-          <div id="adngin-sidebar_left-0"></div>
-        </SidebarAd>
-        <AdContainer>
-          <SocialLinks />
-          <div style={{ textAlign: 'center' }}>
-            <a href="/privacy-policy/" style={{ color: '#df6347' }}>
-              Privacy Policy
-            </a>{' '}
-            -{' '}
-            <div
-              id="ccpa"
-              style={{
-                textAlign: 'center',
-                cursor: 'pointer',
-                display: 'none',
-              }}
-            >
-              Do not share my Personal Information.
+          <LogoContainer href={'/'}>
+            <Logo src={'/images/angle.svg'} alt="logo" />
+            <LogoText>ANGLE</LogoText>
+          </LogoContainer>
+          <IconContainer>
+            <HowToModal />
+            <StatsModal
+              end={end}
+              win={win}
+              guesses={guesses}
+              maxAttempts={MAX_GUESSES}
+              dayString={dayString}
+            ></StatsModal>
+            <LoginModal user={user} logout={logout} />
+          </IconContainer>
+          <Angle
+            angle1={angle1}
+            angle2={angle2}
+            largeArc={deltaAngle > Math.PI}
+          ></Angle>
+          <InputArea>
+            <Input
+              autoFocus
+              type="number"
+              pattern="\d*"
+              onChange={handleInput}
+              onKeyDown={handleEnter}
+              disabled={end}
+              value={guess}
+            />
+            <Button onClick={handleGuess} disabled={end}>
+              Guess!
+            </Button>
+          </InputArea>
+          <Attempts>
+            Attempts:{' '}
+            <span>
+              {guesses.length}/{MAX_GUESSES}
+            </span>
+          </Attempts>
+          <Guesses guesses={guesses} answer={answer} />
+          {!user?.premiumGames?.includes('angle') && (
+            <SidebarAd>
+              <div id="adngin-sidebar_left-0"></div>
+            </SidebarAd>
+          )}
+          <AdContainer>
+            <SocialLinks />
+            <div style={{ textAlign: 'center' }}>
+              <a href="/privacy-policy/" style={{ color: '#df6347' }}>
+                Privacy Policy
+              </a>{' '}
+              -{' '}
+              <div
+                id="ccpa"
+                style={{
+                  textAlign: 'center',
+                  cursor: 'pointer',
+                  display: 'none',
+                }}
+              >
+                Do not share my Personal Information.
+              </div>
             </div>
-          </div>
-          <AdSpace />
-        </AdContainer>
-      </BigContainer>
-    </Main>
+            <AdSpace />
+          </AdContainer>
+        </BigContainer>
+      </Main>
+    </>
   );
 }
 
---
+++ src/ads.js
@@ -0,0 +1,115 @@
+export function loadAds(isPremium) {
+  // set active ad units
+  setActiveAdUnits(isPremium);
+
+  // load ad script
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://cdn.snigelweb.com/adengine/angle.wtf/loader.js';
+  script.type = 'text/javascript';
+  script.setAttribute('data-cfasync', 'false');
+
+  if (!document.querySelector(`script[src*="adengine/angle.wtf/loader.js"]`)) {
+    document.body.appendChild(script);
+  }
+  return script;
+}
+
+function getActiveAdUnits() {
+  let breakpoint = 'xsmall';
+
+  const w = window.innerWidth;
+
+  breakpoint = w >= 768 ? 'small' : breakpoint;
+  breakpoint = w >= 960 ? 'medium' : breakpoint;
+  breakpoint = w >= 1280 ? 'large' : breakpoint;
+  breakpoint = w >= 1400 ? 'xlarge' : breakpoint;
+
+  let activeUnits = [];
+
+  switch (breakpoint.toLowerCase()) {
+    case 'xsmall': // Small Mobile
+    case 'small': // Portrait Tablet / Large Mobile
+      window.activeUnits = ['adhesion', 'top_banner']; // Mobile Ad Units
+      break;
+    default:
+    case 'medium':
+    case 'xlarge':
+    case 'large':
+      window.activeUnits = ['sidebar_left', 'video', 'adhesion'];
+      break;
+  }
+
+  window.snigelPubConf = {
+    adengine: {
+      activeAdUnits: activeUnits,
+    },
+  };
+  return activeUnits;
+}
+
+function setActiveAdUnits(isPremium) {
+  if (isPremium) {
+    window.snigelPubConf = {
+      adengine: {
+        activeAdUnits: [''],
+      },
+    };
+    return;
+  }
+
+  const activeUnits = getActiveAdUnits();
+
+  window.snigelPubConf = {
+    adengine: {
+      activeAdUnits: activeUnits,
+    },
+  };
+}
+
+/**
+ * Check for consent, then load GA and other cookie scripts
+ */
+export const checkForConsent = () => {
+  window.addEventListener('adnginLoaderReady', function () {
+    window.__tcfapi('addEventListener', 2, (tcData, success) => {
+      if (success && tcData?.eventStatus === 'tcloaded') {
+        window.adconsent &&
+          window.adconsent('getConsent', null, function (consent, success) {
+            if (success) {
+              if (consent.fullConsent) {
+                // Load cookie scripts here
+                console.log('########### yas consent');
+                initializeAnalytics();
+              } else {
+                // Otherwise, block all cookie-deploying scripts
+                console.log('########### naur consent');
+              }
+            }
+          });
+      }
+    });
+  });
+};
+
+const initializeAnalytics = () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args) {
+    window.dataLayer?.push(args);
+  }
+
+  const currentID = 'G-WDL5SFD2WC';
+
+  if (!document.querySelector(`script[src*="gtag/js?id=${currentID}"]`)) {
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${currentID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      gtag('js', new Date());
+      gtag('config', currentID);
+    };
+  }
+  return;
+};
