dda9cff15cb557597f87fc3b6a9bfc35e25a106e
emmapejko <emma.pejko@gmail.com>
2025-08-28 12:39:20 -0700

    back to school banner

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import { SocialLinks } from './components/SocialLinks';
 import useUser from './hooks/useUser';
 import AccountsUpdateComponent from './components/AccountsUpdateComponent';
 import { checkForConsent } from './ads';
+import BackToSchoolBanner from './components/backToSchool/BackToSchoolBanner';
 
 const Main = styled.div`
   flex: 1 0 auto;
@@ -226,6 +227,7 @@ function App() {
             transition={Flip}
             autoClose={false}
           />
+          <BackToSchoolBanner />
           <LogoContainer href={'/'}>
             <Logo src={'/images/angle.svg'} alt="logo" />
             <LogoText>ANGLE</LogoText>
